5ac594b33c2551b2ba87783701af08259707756a
Jan Ole Skotterud <j.skotterud@gmail.com>
2013-03-09 21:05:00 +0100

    login/ut

--- App/Register.php
+++ App/Register.php
@@ -6,18 +6,24 @@
             <p>Denne tjenesten hjelper deg til å utvikle de egenskapene du ønsker å forbedre.</p>
             <p>Nyskjerrig? <a href="#howto">Lær mer</a></p>
         </div>
-        <form>
+        <form name="reg">
             <div class="input-group">
                 <div class="input-row">
                     <label>Fornavn</label>
-                    <input type="text" placeholder="Ditt fornavn">
+                    <input type="text" placeholder="Ditt fornavn" name="name" required>
                 </div>
                 <div class="input-row">
                   <label>Epost</label>
-                  <input type="email" placeholder="din@epost.no">
+                  <input type="email" placeholder="din@epost.no" name="email" required>
+                </div>
+
+                <div class="input-row">
+                    <label>Passord</label>
+                    <input type="password" placeholder="passord" name="pass">
                 </div>
             </div>
-            <a data-role="button" class="button-positive">Registrer deg!</a>
+            <a data-role="button" class="button-positive" onclick="registeruser(reg.name.value, reg.pass.value, reg.email.value)">Registrer deg!</a>
+
         </form>
         <ul class="footer-navigation">
             <li><a href="HowTo.php">Lær mer</a></li>
