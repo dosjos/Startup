72875fa5e5258ff905d25d66a4d57ce4b574ecca
Asgeir Hoem <asgeir@stay.com>
2013-03-09 19:20:07 +0100

    Task view dummy content

--- App/Task.php
+++ App/Task.php
@@ -4,7 +4,14 @@
 </header>
 
 <div class="content task">
-    <p>Hvordan styrker du din kreativitet?</p>
+    <div class="lead">
+        <p>Hvordan styrker du din kreativitet? Dette er et engasjerende avsnitt som får deg til å ville starte første del av oppgaven.</p>
+        <div class="proceed">
+            <i class="icon-circle-arrow-down"></i>
+            <p>Klar til å begynne?</p>
+        </div>
+    </div>
+
     <h2>1. Sett mål!</h2>
     <form>
         <div class="input-group">
@@ -37,7 +44,26 @@
               <input type="email" placeholder="male et bilde">
             </div>
         </div>
+        <a class="button-block button-positive">Ok, gå videre!</a>
     </form>
     <h2>2. Visste du at...</h2>
+    <p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.</p>
+
+    <a class="button-block button-positive">Ok, gå videre!</a>
+
     <h2>3. Ta det første steget!</h2>
+    <form>
+        <div class="input-group">
+            <div class="input-row">
+                <input type="checkbox" id="goal1"><label for="goal1">Male huset</label>
+            </div>
+            <div class="input-row">
+                <label>Male noe</label>
+                <div class="toggle">
+                  <div class="toggle-handle"></div>
+                </div>
+            </div>
+        </div>
+        <a class="button-block button-positive">Fullfør!</a>
+    </form>
 </div>
