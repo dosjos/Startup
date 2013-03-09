4e0fb9331b4a76b8b918b67464e431463d9117e1
Jan Ole Skotterud <j.skotterud@gmail.com>
2013-03-09 13:50:55 +0100

    Fikset merge conflict

--- App/index.php
+++ App/index.php
@@ -1,14 +1,13 @@
 <html>
 <head>
     <title>Something something</title>
-<<<<<<< HEAD
+
     <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
-    <script type='text/javascript' src='js/jquery.ba-hashchange.min.js'></script>
-    <script type='text/javascript' src='js/dynamicpage.js'></script>
-=======
+
+
     <link rel="stylesheet/less" type="text/css" href="css/main.less" />
     <script src="js/less.js" type="text/javascript"></script>
->>>>>>> 057bf9c7fb2e1e3d757d968d9361757d22655f53
+
 </head>
 <body>
 <i class="icon-ok"></i>
