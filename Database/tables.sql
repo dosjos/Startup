b4063c84d3eff581e43fcef403f224e25527f7af
Jan Ole Skotterud <j.skotterud@gmail.com>
2013-03-09 12:48:33 +0100

    Added some sql tables

---
+++ Database/tables.sql
@@ -0,0 +1,31 @@
+
+
+
+create table Startup_Users {
+	id int,
+	email varchar(200),
+	name varchar(200),
+	primary key(userid)
+}
+
+create table Starup_Egenskaper
+{
+	id int,
+	egenskap varchar(200),
+	primary key(id)
+}
+
+create table Startup_UsersEgenskaper{
+	id int,
+	userid int not null,
+	egenskapOne int,
+	egenskapTwo int,
+	egenskapThree int,
+	egenskapFour int,
+	egenskapFive int,
+	egenskapSix int,
+	egenskapSeven int,
+	
+	FOREIGN KEY (userid) REFERENCES Startup_Users(id),
+	primary key(id)
+}
