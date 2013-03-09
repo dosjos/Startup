f6a9b0a7a175ade1d6cb13ef3203375018765d23
Jan Ole Skotterud <j.skotterud@gmail.com>
2013-03-09 15:12:31 +0100

    Leker litt med login, og la til tre js biblioteker

--- App/Login.php
+++ App/Login.php
@@ -1,8 +1,31 @@
-<<<<<<< HEAD
-=======
-<header class="bar-title">
-    <h1 class="title">Tester</h1>
-</header>
-    
-<div class="content">sdfhds</div>
->>>>>>> fb3b8f82b053517014cd1fb588abce11bdbb4c2c
+
+<?php
+    if(isset($_POST['epost']) && isset($_POST['pass'])){
+
+
+        ?>
+        <header class="bar-title">
+        <h1 class="title">Logget inn</h1>
+    </header>
+    <?php
+    }else{
+    ?>
+
+
+
+    <header class="bar-title">
+        <h1 class="title">Login</h1>
+    </header>
+
+    <div class="content">
+        <form name="input" action="Login.php" method="post">
+            <div class="input-group">
+                <input type="email" placeholder="Email" name="epost">
+                <input type="password" placeholder="Password" name="pass">
+                <a class="button button-block" href="Login.php">Login</a>
+            </div>
+        </form>
+    </div>
+<?php
+    }
+?>
