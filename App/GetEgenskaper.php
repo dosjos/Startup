20c787d4bb4d0c4f6bf272a0ad116ac26eabf1fe
Jan Ole Skotterud <j.skotterud@gmail.com>
2013-03-09 22:31:17 +0100

    db

--- App/GetEgenskaper.php
+++ App/GetEgenskaper.php
@@ -8,22 +8,26 @@
  */
 
 include("dbsettings.php");
-
-
 $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if(isset($_GET['type'])){
 
-$query = "SELECT * FROM starup_egenskaper";
+    if($_GET['type'] == "egenskaper"){
+        $query = "SELECT * FROM starup_egenskaper";
 
-$result = $mysqli->query($query);
+        $result = $mysqli->query($query);
+        echo "{";
+        while ($row = $result->fetch_array(MYSQLI_NUM)) {
+            echo "egenskap:{\n";
+            echo "id: ". $row[0] ."\n";
+            echo "text: ". $row[1]."\n";
+            echo "}\n";
+        }
 
+        echo "}";
+    }
 
+    if($_GET['type'] == ""){
 
-echo "{";
-while ($row = $result->fetch_array(MYSQLI_NUM)) {
-    echo "egenskap:{\n";
-    echo "id: ". $row[0] ."\n";
-    echo "text: ". $row[1]."\n";
-    echo "}\n";
+    }
+
 }
-
-echo "}";
