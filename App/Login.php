3d110255c33c4e45cc07ee942fc0af8d4609b0b1
Jan Ole Skotterud <j.skotterud@gmail.com>
2013-03-09 14:35:03 +0100

    bla

--- App/Login.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: skottjan
- * Date: 09.03.13
- * Time: 13:00
- * To change this template use File | Settings | File Templates.
- */
--- App/Register.php
+++ App/Register.php
@@ -6,7 +6,14 @@
 <header class="bar-title">
     <h1 class="title">Title</h1>
 </header>
-
+<div class="content">
+    <form>
+        <div class="input-group">
+            <input type="text" placeholder="Navn">
+            <input type="email" placeholder="E-post">
+        </div>
+    </form>
+</div>
 
 
 </body>
