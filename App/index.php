2bda0f29b1ba6400199040c66bf4ea5c4c5a6e0a
Jan Ole Skotterud <j.skotterud@gmail.com>
2013-03-09 13:06:36 +0100

    Added a couple of files

---
+++ App/index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Something something</title>
+
+</head>
+<body>
+<?php
+    echo "<div class='MainWrapper'>";
+
+        echo "<div class='Login'>Login</div>";
+
+        echo "<div class='Register'>Register</div>";
+
+    echo "</div>";
+
+
+?>
+</body>
+</html>
