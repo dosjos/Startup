f61456b4cea2a51611eb22158938742869b94676
Jan Ole Skotterud <j.skotterud@gmail.com>
2013-03-10 11:13:52 +0100

    db jobbing

--- App/GetEgenskaper.php
+++ App/GetEgenskaper.php
@@ -39,8 +39,34 @@ if(isset($_GET['type'])){
         echo "}";
     }
 
-    if($_GET['type'] == ""){
+    //?type=getman&id=4
+    if($_GET['type'] == "getman"){
+        $id = $_GET["id"];
+        $query = "SELECT * FROM Startup_UsersEgenskaper";
 
+        $result = $mysqli->query($query);
+        $row = $result->fetch_array(MYSQLI_NUM);
+
+        echo "{";
+        echo '"id": '. $row[0] .",\n";
+        echo '"id": '. $row[2] .",\n";
+        echo '"id": '. $row[3] .",\n";
+        echo '"id": '. $row[4] .",\n";
+        echo '"id": '. $row[5] .",\n";
+        echo '"id": '. $row[6] .",\n";
+        echo '"id": '. $row[7] .",\n";
+        echo '"id": '. $row[8] .",\n";
+        echo "}";
     }
+
+
+
+    //$result = mysql_query("SELECT id FROM stats WHERE zone=$zone AND date=$today LIMIT 1");
+    //if(mysql_num_rows($result)) {
+    //    $id = mysql_result($result,0);
+    //    mysql_query("UPDATE stats SET hits=hits+1 WHERE id=$id");
+    //} else {
+    //    mysql_query("INSERT INTO stats (zone, date, hits) VALUES ($zone, $today, 1)");
+    //}
 
 }
