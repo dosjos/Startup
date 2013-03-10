a87d9ad16a281c831f38ba980ebf9ca3ba348d80
Design Container <dcmac5@Tommy.local>
2013-03-10 13:49:49 +0100

    tommy

--- App/Home.php
+++ App/Home.php
@@ -20,9 +20,9 @@
 
             <ul class="properties">
                 <li class="head"><a href="#select" data-rel="popup" data-position-to="window">Velg</a></li>
-                <li class="left-arm selected"><a href="#select" data-rel="popup" data-position-to="window">Velg</a></li>
+                <li class="left-arm"><a href="#select" data-rel="popup" data-position-to="window">Velg</a></li>
                 <li class="right-arm"><a href="#select" data-rel="popup" data-position-to="window">Velg</a></li>
-                <li class="torso completed"><a href="#select" data-rel="popup" data-position-to="window">Velg</a></li>
+                <li class="torso"><a href="#select" data-rel="popup" data-position-to="window">Velg</a></li>
                 <li class="left-leg"><a href="#select" data-rel="popup" data-position-to="window">Velg</a></li>
                 <li class="right-leg"><a href="#select" data-rel="popup" data-position-to="window">Velg</a></li>
             </ul>
@@ -32,6 +32,24 @@
 		<div data-role="popup" id="select">
 		        <ul data-role="listview" data-inset="true" class="ui-icon-alt" data-theme="d" >
 		            <li class="listdesc" data-role="divider">Velg en egenskap:</li>
+					<li>Kreativitet</li>
+					<li>Humoristisk sans</li>
+					<li>Være ekte</li>
+					<li>Nysgjerrighet</li>
+					<li>Like å lære</li>
+					<li>Modig</li>
+					<li>Håp</li>
+					<li>Kjærlighet</li>
+					<li>Entusiasme</li>
+					<li>Utholdenhet</li>
+					<li>Være snill</li>
+					<li>Rettferdighet</li>
+					<li>Jordnærhet</li>
+					<li>Viljestyrke</li>
+					<li>Sosial intelligens</li>
+					<li>Estetisk sans</li>
+					<li>Takknemlighet</li>
+					<li>Tilgivelse</li>
 		        </ul>
 		</div>
 
